43d0eb878763b67bfa8967cf8a5368b604b43bb3
btxzybgeyf7827 <126921796+btxzybgeyf7827@users.noreply.github.com>
2023-04-11 21:04:15 +0800

    Add files via upload

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,34 @@
-const { defineConfig } = require('@vue/cli-service')
+const {
+  defineConfig
+} = require('@vue/cli-service')
 const path = require('path')
+// element-plus
+const AutoImport = require('unplugin-auto-import/webpack')
+const Components = require('unplugin-vue-components/webpack')
+const {
+  ElementPlusResolver
+} = require('unplugin-vue-components/resolvers')
+
 module.exports = defineConfig({
   transpileDependencies: true,
-  lintOnSave:false,
+  configureWebpack: {
+    plugins: [
+      AutoImport({
+        resolvers: [ElementPlusResolver()],
+      }),
+      Components({
+        resolvers: [ElementPlusResolver()],
+      }),
+    ],
+  },
+  lintOnSave: false,
   pluginOptions: {
     'style-resources-loader': {
-        preProcessor: 'less',
-        patterns: [
-          path.join(__dirname, './src/assets/styles/variables.less'),
-          path.join(__dirname, './src/assets/styles/mixins.less')
-        ]
+      preProcessor: 'less',
+      patterns: [
+        path.join(__dirname, './src/assets/styles/variables.less'),
+        path.join(__dirname, './src/assets/styles/mixins.less')
+      ]
     }
-},
+  }
 })
